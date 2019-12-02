396ec9fe9b73123bf3cccd2b97424243d21bc566
Gerbrand De Laender <gdelaende@gmail.com>
2019-12-02 12:46:15 +0100

    Disabled zsh support when checking for piped output.

--- evertims/build/build_win64.sh
+++ evertims/build/build_win64.sh
@@ -141,7 +141,7 @@ function build_evertims
   make | indent
 
   # See https://unix.stackexchange.com/questions/14270/get-exit-status-of-process-thats-piped-to-another.
-  if [ ${PIPESTATUS[0]} -gt 0 ] || [ ${pipestatus[1]} -gt 0 ]
+  if [ ${PIPESTATUS[0]} -gt 0 ]
   then
     report_failure
   fi
